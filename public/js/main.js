4aab1641ee21da38d20ee07fac94ed0965b59c1d
Semi-dios <tipouno@misena.edu.co>
2021-09-10 21:16:01 -0500

    Prueba

--- public/js/main.js
+++ public/js/main.js
@@ -68832,8 +68832,8 @@ try {
 
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js"); //Active into heroku
-//window.axios.defaults.baseURL = 'http://entregables.herokuapp.com/public/';
 
+window.axios.defaults.baseURL = 'http://entregables.herokuapp.com/public/';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
